03c3f3dc669aa04b7044b0f506a7d4ace65bea4f
Tejas <trao1011@outlook.com>
2024-08-08 17:55:47 -0700

    Add unhygienic macro expander simulating syntax-rules.

---
+++ ch9a.rkt
@@ -0,0 +1,96 @@
+#lang racket/base
+(require test-engine/racket-tests)
+; An unhygienic macro expander from https://theincredibleholk.org/blog/2013/02/12/patterns-with-ellipses/
+
+(define (match* p e literals sk fk)
+  (cond
+    ((and (pair? p) (pair? (cdr p)) (eq? '... (cadr p)))
+     (let loop ((e e)
+                (b '()))
+       (if (null? e)
+           (match* (cddr p) e literals
+             (lambda (b^) (sk `((... . ,b) . ,b^)))
+             fk)
+           (match* (car p) (car e) literals
+             (lambda (b^)
+               (loop (cdr e) (append b (list b^))))
+             (lambda ()
+               (match* (cddr p) e literals
+                 (lambda (b^) (sk `((... . ,b) . ,b^)))
+                 fk))))))
+    ((and (pair? p) (pair? e))
+     (match* (car p) (car e) literals
+       (lambda (b)
+         (match* (cdr p) (cdr e) literals
+           (lambda (b^) (sk (append b b^)))
+           fk))
+       fk))
+    ((or (eq? p '_))
+     (sk '()))
+    ((and (member p literals) (eq? p e))
+     (sk '()))
+    ((symbol? p)
+     (sk (list (cons p e))))
+    ((and (null? p) (null? e))
+     (sk '()))
+    (else (fk))))
+
+(define (mem* x ls)
+  (cond
+    ((and (pair? ls) (pair? (cdr ls)) (eq? (cadr ls) '...))
+     #f)
+    ((pair? ls)
+     (or (mem* x (car ls)) (mem* x (cdr ls))))
+    (else (eq? x ls))))
+(define (extract-... p bindings)
+  (if (null? bindings)
+      '()
+      (let ((rest (extract-... p (cdr bindings)))
+            (b (car bindings)))
+        (if (and (eq? (car b) '...) (not (null? (cdr b))))
+            (let ((names (map car (cadr b))))
+              (if (ormap (lambda (x) (mem* x p)) names)
+                  (cons (cdr b) rest)
+                  rest))
+            rest))))
+(define (instantiate* p bindings)
+  (cond
+    ((and (pair? p) (pair? (cdr p)) (eq? '... (cadr p)))
+     (let ((bindings... (extract-... (car p) bindings)))
+       (if (null? bindings...)
+           (instantiate* (cddr p) bindings)
+           (append
+            (apply map (cons (lambda b*
+                               (instantiate* (car p)
+                                             (append (apply append b*)
+                                                     bindings)))
+                             bindings...))
+            (instantiate* (cddr p) bindings)))))
+    ((pair? p)
+     (cons (instantiate* (car p) bindings)
+           (instantiate* (cdr p) bindings)))
+    ((assq p bindings) => cdr)
+    (else p)))
+
+(check-expect (match* '(let ((x e) ...) b) '(let ((x 5) (y 6)) (+ x y)) '(let) (lambda (b) b) (lambda () #f)) '((... ((x . x) (e . 5)) ((x . y) (e . 6))) (b . (+ x y))))
+(check-expect (match* '(_ e1 e2) '(or2 1 2) '() (lambda (b) b) (lambda () #f)) '((e1 . 1) (e2 . 2)))
+(check-expect (match* '(_ e1 e2 . e3) '(thing 1 2 3 4 5 6) '() (lambda (b) b) (lambda () #f)) '((e1 . 1) (e2 . 2) (e3 3 4 5 6)))
+(check-expect (match* '(let ((name val) ...) body1 body2 ...) '(let ((x 5) (y 6)) (set! a 4) (+ x y)) '(let)
+                (lambda (b) (instantiate* '((lambda (name ...) body1 body2 ...) val ...) b))
+                (lambda () #f))
+              '((lambda (x y) (set! a 4) (+ x y)) 5 6))
+
+; Unhygienic case
+(check-expect
+ (let ((t 5)) ; evaluate an expression, using...
+   (or #f t)) ; the expansion of this builtin macro using Racket's own system
+ 5)           ; expecting a result consistent with hygienic macro expansion
+(check-expect
+ (let ((mexp (match* '(_ e1 e2) '(or #f t) '()                           ; the macro match
+               (lambda (b) (instantiate* '(let ((t e1)) (if t t e2)) b)) ; the macro expansion
+               (lambda () #f))))         
+   (let ((fexp `(let ((t 5)) ,mexp)))                                    ; create an expression using the macro expansion
+     (eval fexp (make-base-namespace))))                                 ; evaluate that expression
+ #f)                                                                     ; expecting a result consistent with unhygienic macro expansion
+
+(test)
