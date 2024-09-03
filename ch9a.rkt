31d40064b372b8080370f230450be5c76b609e5b
Tejas <trao1011@outlook.com>
2024-09-02 17:19:26 -0700

    remove snoc

--- ch9a.rkt
+++ ch9a.rkt
@@ -2,32 +2,21 @@
 (require test-engine/racket-tests)
 ; An unhygienic macro expander from https://theincredibleholk.org/blog/2013/02/12/patterns-with-ellipses/
 
-(define (match* p e literals sk fk)
+(define (match* p e sk fk)
   (cond
     ((and (pair? p) (pair? (cdr p)) (eq? '... (cadr p)))
      (let loop ((e e)
                 (b '()))
        (if (null? e)
-           (match* (cddr p) e literals
-             (lambda (b^) (sk `((... . ,b) . ,b^)))
-             fk)
-           (match* (car p) (car e) literals
-             (lambda (b^)
-               (loop (cdr e) (append b (list b^))))
-             (lambda ()
-               (match* (cddr p) e literals
-                 (lambda (b^) (sk `((... . ,b) . ,b^)))
-                 fk))))))
+           (match* (cddr p) e (lambda (b+) (sk (cons (cons '... (reverse b)) b+))) fk)
+           (match* (car p) (car e)
+             (lambda (b+) (loop (cdr e) (cons b+ b))) ; builds each ellipsis item, but in reverse (for speed), reversed when iteration finishes
+             (lambda () (match* (cddr p) e (lambda (b+) (sk (cons (cons '... b) b+))) fk))))))
     ((and (pair? p) (pair? e))
-     (match* (car p) (car e) literals
-       (lambda (b)
-         (match* (cdr p) (cdr e) literals
-           (lambda (b^) (sk (append b b^)))
-           fk))
+     (match* (car p) (car e)
+       (lambda (b) (match* (cdr p) (cdr e) (lambda (b+) (sk (append b b+))) fk))
        fk))
     ((or (eq? p '_))
-     (sk '()))
-    ((and (member p literals) (eq? p e))
      (sk '()))
     ((symbol? p)
      (sk (list (cons p e))))
@@ -72,10 +61,10 @@
     ((assq p bindings) => cdr)
     (else p)))
 
-(check-expect (match* '(let ((x e) ...) b) '(let ((x 5) (y 6)) (+ x y)) '(let) (lambda (b) b) (lambda () #f)) '((... ((x . x) (e . 5)) ((x . y) (e . 6))) (b . (+ x y))))
-(check-expect (match* '(_ e1 e2) '(or2 1 2) '() (lambda (b) b) (lambda () #f)) '((e1 . 1) (e2 . 2)))
-(check-expect (match* '(_ e1 e2 . e3) '(thing 1 2 3 4 5 6) '() (lambda (b) b) (lambda () #f)) '((e1 . 1) (e2 . 2) (e3 3 4 5 6)))
-(check-expect (match* '(let ((name val) ...) body1 body2 ...) '(let ((x 5) (y 6)) (set! a 4) (+ x y)) '(let)
+(check-expect (match* '(let-id ((x e) ...) b) '(let ((x 5) (y 6)) (+ x y)) (lambda (b) b) (lambda () #f)) '((let-id . let) (... ((x . x) (e . 5)) ((x . y) (e . 6))) (b . (+ x y))))
+(check-expect (match* '(_ e1 e2) '(or2 1 2) (lambda (b) b) (lambda () #f)) '((e1 . 1) (e2 . 2)))
+(check-expect (match* '(_ e1 e2 . e3) '(thing 1 2 3 4 5 6) (lambda (b) b) (lambda () #f)) '((e1 . 1) (e2 . 2) (e3 . (3 4 5 6))))
+(check-expect (match* '(let ((name val) ...) body1 body2 ...) '(let ((x 5) (y 6)) (set! a 4) (+ x y))
                 (lambda (b) (instantiate* '((lambda (name ...) body1 body2 ...) val ...) b))
                 (lambda () #f))
               '((lambda (x y) (set! a 4) (+ x y)) 5 6))
@@ -86,7 +75,7 @@
    (or #f t)) ; the expansion of this builtin macro using Racket's own system
  5)           ; expecting a result consistent with hygienic macro expansion
 (check-expect
- (let ((mexp (match* '(_ e1 e2) '(or #f t) '()                           ; the macro match
+ (let ((mexp (match* '(_ e1 e2) '(or #f t)                               ; the macro match
                (lambda (b) (instantiate* '(let ((t e1)) (if t t e2)) b)) ; the macro expansion
                (lambda () #f))))         
    (let ((fexp `(let ((t 5)) ,mexp)))                                    ; create an expression using the macro expansion
